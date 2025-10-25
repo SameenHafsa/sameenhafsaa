c8843f52e369ff3f38c2fd7a53dc7603fe7a9233
Sameen <hafsasamen@gmail.com>
2025-10-25 03:28:03 -0500

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
-  base: '/portfolio-sameen/',
+  base: '/sameenhafsaa/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
